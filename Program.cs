758ed04a2023d66af15933580a5ab3d0912dfd97
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 21:33:15 -0300

    minhas bagas

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        Console.WriteLine("\nSummertime Sadness\n");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(@"
+        Oh my God, I feel it in the air
+        Telephone wires above are sizzlin' like a snare
+        Honey, I'm on fire, I feel it everywhere
+        Nothin' scares me anymore (one, two, three, four)
+        ");
+
+        Console.WriteLine("\n");
+        
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(@"
+       Kiss me hard before you go
+       Summertime sadness
+       I just wanted you to know
+       That, baby, you the best
+        ");
+        Console.ResetColor();
+        
+        Console.WriteLine("\n");
+        
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(@"
+        I got that summertime, summertime sadness
+        Su-su-summertime, summertime sadness
+        Got that summertime, summertime sadness
+        Oh, oh-oh, oh-oh
+        ");
+        Console.ResetColor();
+
+        Console.WriteLine("\n");
+
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(@"
+        'm feelin' electric tonight
+        Cruisin' down the coast, goin' 'bout 99 
+        Got my bad baby by my heavenly side
+        I know if I go, I'll die happy tonigh.
+        ");
+        Console.ResetColor();
